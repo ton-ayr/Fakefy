904f402e3ebef91a081a9870ac09534e66a1fe85
ton-ayr <ayrtoncbarreira@gmail.com>
2025-02-18 20:12:00 -0300

    Atualização para deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const URL = "http://localhost:3000/api";
+//const URL = "http://localhost:3000/api";
+
+const URL = "https://fakefy.onrender.com/";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
